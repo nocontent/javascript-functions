809bc87a42f9ca7b38a41fde50c14b4a4add97a7
Patrick <1243160+nocontent@users.noreply.github.com>
2022-09-24 21:27:23 +0000

    Update should have a `same` function.

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -3,7 +3,7 @@ function seed(a,b,c) {
 }
 
 function same([x, y], [j, k]) {
-  return arguments[0].every((element, index) => element == arguments[1][index]);
+  return x === j && y === k;
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
